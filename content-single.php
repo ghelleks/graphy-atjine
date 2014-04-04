668c631d17b0b47ebdf07c6967b02e9fe4a3352c
Gunnar Hellekson <gunnar@hellekson.com>
2014-04-04 03:42:23 +0000

    added support for twitter posts

--- content-single.php
+++ content-single.php
@@ -12,7 +12,20 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+<?php  /* Here comes the twitter hack that is insanely specific to me, but mostly harmless */
+	if ( get_post_format() === 'status' )   {
+		$tweet_id = get_post_custom_values('tweet_id');
+		if ($tweet_id) {
+			$url = 'https://twitter.com/#!/twitter/status/' . $tweet_id[0];
+		}
+		else {
+			$url = trim(strip_tags(get_the_content()));
+		}
+		echo wp_oembed_get($url);
+?> 
+<?php } else { ?>
 		<?php the_content(); ?>
+<?php } ?>
 		<?php wp_link_pages( array(	'before' => '<div class="page-links">' . __( 'Pages:', 'graphy' ), 'after'  => '</div>', ) ); ?>
 	</div><!-- .entry-content -->
 	<?php graphy_footer_meta(); ?>
